971cad55f14bcc5d5c5077afe4e5f425fdb32add
CyanicYang <cyanic.yangyu@gmail.com>
2019-06-07 15:48:31 +0800

    add database & change types

--- server/include/types.hpp
+++ server/include/types.hpp
@@ -61,6 +61,62 @@ struct Packet {
     DynamicPayload payload;
 };
 
+enum class PacketType: uint8_t {
+    VersionRequire = 0x00,
+    
+    AuthRequest = 0x01,
+    AuthResponse = 0x01,
+    
+    SysInfoRequest = 0x02,
+    SysInfoResponse = 0x02,
+    
+    ConfInfoRequest = 0x03,
+    ConfInfoResponse = 0x03,
+    
+    ProcInfoRequest = 0x04,
+    ProcInfoResponse = 0x04,
+    
+    EtherInfoRequest = 0x05,
+    EtherInfoResponse = 0x05,
+    
+    USBInfoRequest = 0x07,
+    USBInfoResponse = 0x07,
+    USBfileRequest = 0x0C,
+    USBfileResponse = 0x0C,
+    
+    PrintDevRequest = 0x08,
+    PrintDevResponse = 0x08,
+    PrintQueueRequest = 0x0D,
+    PrintQueueResponse = 0x0D,
+
+    TerInfoRequest = 0x09,
+    TerInfoResponse = 0x09,
+    DumbTerRequest = 0x0A,
+    DumbTerResponse = 0x0A,
+    IPTermRequest = 0x0B,
+    IPTermResponse = 0x0B,
+
+    End = 0xFF
+};
+
+struct VersionRequirePacket {
+    uint8_t direction;
+    uint8_t descriptor;
+    uint16_t packet_size;
+    uint8_t pad_1[2];
+    uint16_t payload;
+    uint16_t version_require;
+    uint8_t version_sub1;
+    uint8_t version_sub2;
+    uint16_t time_gap_fail;
+    uint16_t time_gap_succeed;
+    uint8_t is_empty_tty;
+    uint8_t pad[3];
+    uint8_t auth_string[32];
+    uint32_t random_num;
+    uint32_t svr_time;
+};
+
 // Used as a buffer in transfer layer, instantiated in Clients
 class CircularQueue {
 public:
